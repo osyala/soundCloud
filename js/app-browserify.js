3e5e3145d97234158cdb9bd6cca3a454bb838789
Oscar Ayala <OscarAyala@Oscars-MacBook-Air.local>
2015-10-14 21:28:53 -0500

    create new repo

---
+++ js/app-browserify.js
@@ -0,0 +1,18 @@
+// es5 and 6 polyfills, powered by babel
+require("babel/polyfill")
+
+let fetch = require('./fetcher')
+
+// other stuff that we don't really use in our own code
+// var Pace = require("../bower_components/pace/pace.js")
+
+// require your own libraries, too!
+// var Router = require('./app.js')
+
+// window.addEventListener('load', app)
+
+// function app() {
+    // start app
+    // new Router()
+// }
+
